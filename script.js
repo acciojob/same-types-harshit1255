75ec706a3cd2295c121fa548c88938fe9967bd91
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-26 00:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
-}
+	return typeof(value1) ==typeof(value2)
+} 
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
